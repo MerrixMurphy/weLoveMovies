8a4dc7dc0482d4e35079396e390ad197b969262c
Merrix Murphy <merrixadmurphy@gmail.com>
2021-10-18 21:36:41 -0700

    Cleaned up Reviews

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -1,6 +1,5 @@
 const reviewsService = require("./reviews.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
-const reduceProp = require("../utils/reduce-properties");
 
 async function correctId(req, res, next) {
   const review = await reviewsService.read(req.params.reviewId);
@@ -14,14 +13,6 @@ async function correctId(req, res, next) {
   });
 }
 
-const reducedCritics = reduceProp("critic_id", {
-  critic_id: ["critic", null, "critic_id"],
-  review_id: ["critic", null, "review_id"],
-  preferred_name: ["critic", null, "preferred_name"],
-  surname: ["critic", null, "surname"],
-  organization_name: ["critic", null, "organization_name"],
-});
-
 //figure out why update returns undefined
 async function update(req, res, next) {
   const toUpdate = {
@@ -31,6 +22,7 @@ async function update(req, res, next) {
   };
   const updatedReview = await reviewsService.update(toUpdate);
   toUpdate.critic = updatedReview;
+  console.log(toUpdate);
   res.json({ data: toUpdate });
 }
 
